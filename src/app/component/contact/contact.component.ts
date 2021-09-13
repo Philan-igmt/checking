0ae77507895d11d35d1d6e6f2bc55136c4ad6329
Philan-igmt <philanisithembiso@gmail.com>
2021-09-13 19:48:29 +0200

    contact us form validation

--- src/app/component/contact/contact.component.ts
+++ src/app/component/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl,Validators, FormArray } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  contactForm: FormGroup = new FormGroup({
+    name: new FormControl(),
+    email: new FormControl(),
+    message: new FormControl(),
+  });
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      message:["",[Validators.required,]],
+    });
+
   }
 
 }
